d52e9b356c70b0fcb9c359642cdfd1bc66d12081
Luca Denti <ldenti92@gmail.com>
2020-01-17 18:31:14 +0100

    Initial polishing for genexplvprofile.py script

--- src/genexplvprofile.py
+++ src/genexplvprofile.py
@@ -33,144 +33,151 @@ HISTORY
 	  Initialization.
 '''
 
-import sys
+import sys, os
+import argparse
 import pydoc
-import os
-import re
 import fileinput
 import random
+
 import numpy
 
 def parsebed(lines):
   # Parse one line in count data
-  fd=lines.strip().split('\t')
-  if len(fd)!=12:
-    return ['',-1,-1,0]
-  if fd[10].endswith(','):
-    fd[10]=fd[10][:-1]
-  if fd[11].endswith(','):
-    fd[11]=fd[11][:-1]
-  seglen=[int(x) for x in fd[10].split(',')]
-  segstart=[int(x) for x in fd[11].split(',')]
-  #jstart=int(fd[1])+seglen[0]+1
-  #jend=int(fd[1])+segstart[1]+1
-  jstart=int(fd[1])+1 # start is 0-base; increase 1 to convert to 1-base
-  jend=int(fd[2])
-  # jscore=int(fd[4])
-  #seg1=[jstart+segstart[i] for i in range(len(segstart))]
-  #seg2=[jstart+segstart[i]+seglen[i]-1 for i in range(len(segstart))]
+  fd = lines.strip().split('\t')
+
+  if len(fd) != 12:
+    return ['',-1,-1,0,-1,-1,-1]
+
+  jstart = int(fd[1])+1 # start is 0-base; increase 1 to convert to 1-base
+  jend = int(fd[2])
+  fd[10] = fd[10][:-1] if fd[10][-1] == ',' else fd[10]
+  seglen = [int(x) for x in fd[10].split(',')]
+
+  # fd[11] = fd[11][:-1] if fd[11][-1] == ',' else fd[11]
+  # segstart = [int(x) for x in fd[11].split(',')]
+  # jstart = int(fd[1])+seglen[0]+1
+  # jend = int(fd[1])+segstart[1]+1
+  # jscore = int(fd[4])
+  # seg1 = [jstart+segstart[i] for i in range(len(segstart))]
+  # seg2 = [jstart+segstart[i]+seglen[i]-1 for i in range(len(segstart))]
   # [seg1,seg2] are now 1-base inclusive
+
   return [fd[0],jstart,jend,fd[3],sum(seglen),fd[5],fd[9]]
 
-argvi=1
-mindist=50
-minscore=2
-mu=-4
-sigma=4
-assignexplv=True
+def is_valid_geometric(parser, arg):
+  try:
+    arg = float(arg)
+    if not 0.0 <= arg <= 1.0:
+      raise ValueError
+  except ValueError:
+    parser.error("Geometric parameter prob must be a float value in range [0.0, 1.0]")
+  return arg
 
+def main():
+  parser = argparse.ArgumentParser(description="Assign expression level to a set of transcripts.")
 
-allfile=[]
+  # Optional arguments
+  parser.add_argument("-f", "--statonly",
+                      dest="assignexplv",
+                      help="Print the statistics only; do not assign expression levels.",
+                      required=False,
+                      action="store_false",
+                      default=True)
 
-distype="lognormal"
+  parser.add_argument("-e", "--lognormal",
+                      dest="lognormal",
+                      help="Specify the mean mu and variance sigma of the lognormal distribution used to assign expression levels. Default -4.0 4.0",
+                      required=False,
+                      nargs=2,
+                      metavar=('mu', 'sigma'),
+                      type = float,
+                      action="store",
+                      default=[-4,4])
 
-while argvi <(len(sys.argv)):
-  if sys.argv[argvi]=="-h" or sys.argv[argvi]=="--help" :
-    print(pydoc.render_doc(sys.modules[__name__]),file=sys.stderr)
-    sys.exit();
-  elif sys.argv[argvi]=="-f" or sys.argv[argvi]=="--statonly":
-    assignexplv=False
-  elif sys.argv[argvi]=="-e" or sys.argv[argvi]=="--lognormal" :
-    distype="lognormal"
-    ms=sys.argv[argvi+1].split(",")
-    argvi=argvi+1
-    if len(ms)!=2:
-      print('Error: incorrect parameter for -e.',file=sys.stderr)
-      sys.exit()
-    try:
-      mu=float(ms[0])
-      sigma=float(ms[1])
-    except ValueError:
-      print('Error: incorrect parameter for -e.',file=sys.stderr)
-      sys.exit()
-    print('Mean and variance for lognormal distribution: '+str(mu)+','+str(sigma),file=sys.stderr)
-  elif sys.argv[argvi]=="--geometric":
+  parser.add_argument("-g", "--geometric",
+                      dest="geometric",
+                      help="Use geometric distribution with parameter prob (instead of lognormal) to assign expression levels. Default: None",
+                      required=False,
+                      metavar='prob',
+                      type = lambda x: is_valid_geometric(parser, x),
+                      action="store",
+                      default=None)
+
+  # Positional arguments
+  parser.add_argument("bed_paths", nargs='+', help="Path to bed file")
+
+  args = parser.parse_args()
+
+  distype="lognormal"
+  mu, sigma = args.lognormal
+  p = args.geometric
+  if p is not None:
     distype="geometric"
-    try:
-      mu=float(sys.argv[argvi+1])
-      if mu<0 or mu>1:
-        print('Error: the parameter for geometric distribution must be between 0 and 1.',file=sys.stderr)
-        sys.exit()
-    except ValueError:
-      print('Error: incorrect parameter for -e.',file=sys.stderr)
-      sys.exit()
-    print('Mean for geometric distribution: '+str(mu),file=sys.stderr)
-    argvi=argvi+1
+    print("Using geometric distribution with p={}".format(p), file=sys.stderr, end=' ')
   else:
-    allfile.append(sys.argv[argvi])
-  argvi=argvi+1
+    print("Using lognormal distribution with mu={} and sigma={}".format(mu, sigma), file=sys.stderr, end=' ')
+  print("on {}.".format(','.join(args.bed_paths)), file=sys.stderr)
 
+  print("#ID\tLength\tDir\tExons\tPosition\tGroupID\tNIsoformInGroup", end='')
+  if args.assignexplv:
+    print("\tExplv")
+  else:
+    print("")
 
-allid={}
+  prevchr, prevstart, prevend = "", 0, 0
 
-prevchr=""
-prevrange=[0,0]
-rangeid=0
+  rangeid = 0
+  currentgene = []
+  groupid = 0
+  nline = 0
+  for line in fileinput.input(args.bed_paths):
+    nline += 1
+    chrname, jstart, jend, idx, length, direction, nexon = parsebed(line)
+    if len(chrname)==0 and jstart<0:
+      print("Skipping line {} (malformed)".format(nline), file = sys.stderr)
+      continue
 
-nline=0
-
-currentgene=[]
-groupid=0
-
-print('#ID\tLength\tDir\tExons\tPosition\tGroupID\tNIsoformInGroup',end='')
-if assignexplv==True:
-  print('\tExplv')
-else:
-  print()
-
-for lines in fileinput.input(allfile):
-  nline=nline+1
-  pf=parsebed(lines)
-  chrname=pf[0];jstart=pf[1];jend=pf[2];id=pf[3]
-  if len(chrname)==0 and jstart<0:
-    continue
-  length=pf[4];direction=pf[5];nexon=pf[6]
-  if chrname!=prevchr or jstart-prevrange[1]>0:
-    if len(prevchr)!=0:
-      groupid=groupid+1
-      for item in currentgene:
-        print(item[0]+"\t"+str(groupid)+"\t"+str(len(currentgene)),end='')
-        if assignexplv==True:
-          if distype=="geometric":
-           weight=numpy.random.geometric(mu)*item[1]
+    # A group is a set of overlapping isoforms from the same chromosome
+    if chrname != prevchr or jstart > prevend:
+      # We parse the current group
+      if len(prevchr) != 0:
+        groupid += 1
+        for item in currentgene:
+          print(item[0] + '\t' + str(groupid) + '\t' + str(len(currentgene)), end='')
+          if args.assignexplv:
+            if distype == "geometric":
+              weight = numpy.random.geometric(mu)*item[1]
+            else:
+              weight = random.lognormvariate(mu,sigma)*item[1]
+            print("\t" + str(weight))
           else:
-            weight=random.lognormvariate(mu,sigma)*item[1]
-          print("\t"+str(weight))
-        else:
-          print()
-    prevrange[0]=jstart
-    prevrange[1]=jend
-    prevchr=chrname
-    rangeid=rangeid+1
-    currentgene=[]
-  elif jstart<prevrange[0]:
-    print('Warning: the range is not sorted at line '+str(nline),file=sys.stderr)
-  else:
-    if jend>prevrange[1]:
-      prevrange[1]=jend
-  currentgene.append((id+"\t"+str(length)+"\t"+direction+"\t"+str(nexon)+"\t"+chrname+":"+str(jstart)+"-"+str(jend),length))
-
-
-if len(prevchr)!=0:
-  groupid=groupid+1;
-  for item in currentgene:
-    print(item[0]+"\t"+str(groupid)+"\t"+str(len(currentgene)),end='')
-    if assignexplv==True:
-      if distype=="geometric":
-        weight=numpy.random.geometric(mu)*item[1]
-      else:
-        weight=random.lognormvariate(mu,sigma)*item[1]
-      print("\t"+str(weight))
+            print("")
+      prevstart = jstart
+      prevend = jend
+      prevchr = chrname
+      rangeid = rangeid+1
+      currentgene = []
+    elif jstart < prevstart:
+      print("Warning: the range is not sorted at line {}".format(nline), file=sys.stderr)
     else:
-      print()
+      if jend > prevend:
+        prevend = jend
+    currentgene.append((idx+"\t"+str(length)+"\t"+direction+"\t"+str(nexon)+"\t"+chrname+":"+str(jstart)+"-"+str(jend),length))
 
+
+  # Here we parse the last group not parsed in the for
+  if len(prevchr)!=0:
+    groupid += 1;
+    for item in currentgene:
+      print(item[0]+"\t"+str(groupid)+"\t"+str(len(currentgene)),end='')
+      if args.assignexplv:
+        if distype=="geometric":
+          weight=numpy.random.geometric(mu)*item[1]
+        else:
+          weight=random.lognormvariate(mu,sigma)*item[1]
+        print("\t"+str(weight))
+      else:
+        print("")
+
+if __name__ == "__main__":
+  main()
